b85b1e4cc38fba5ac88ced094c107297b7f8784d
Alexander Lazarev <Alexander_Lazarev@epam.com>
2017-08-02 13:05:33 +0300

    mes

--- src/main/java/com/test/Project.java
+++ src/main/java/com/test/Project.java
@@ -10,9 +10,10 @@ class Project {
   }
   
   public static void main(String[] args) {
-    @Notnull
-    String n;
-    n = null;
+    AtomicInteger aInt1 = new AtomicInteger(0);
+    AtomicInteger aInt2 = new AtomicInteger(0);
+    if (aInt1.equals(aInt2)) {;}
+
     System.out.println("Hellow MTN");
   }
 
